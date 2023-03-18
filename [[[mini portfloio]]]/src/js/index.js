34c7b881297bf0f73adcb0214f438bf1c9063df3
Isaias Dias <116515507+ZanzimD@users.noreply.github.com>
2023-03-18 12:16:33 -0300

    Add files via upload
    update

---
+++ [[[mini portfloio]]]/src/js/index.js
@@ -0,0 +1,32 @@
+
+const abas = document.querySelectorAll(".aba"); 
+
+
+abas.forEach((aba) => {
+    aba.addEventListener("click", function(){
+        if (aba.classList.contains("selecionado")) return;
+
+       selecionarAba(aba);
+       mostrarInformacoesDaAba(aba);
+
+    });
+});
+
+function selecionarAba(aba) {
+    //passo 3:
+    const abaSelecionada = document.querySelector(".aba.selecionado");
+    abaSelecionada.classList.remove("selecionado");
+    //passo 4:
+    aba.classList.add("selecionado");
+}
+
+function mostrarInformacoesDaAba(aba){
+    
+    const idDoElementoDeInformacoesDaAba = `informacao-${aba.id}`;
+    const informacaoASerMostrada = document.getElementById(idDoElementoDeInformacoesDaAba);
+    //passo 5:
+    const informacaoSelecionada = document.querySelector(".informacao.selecionado");
+    informacaoSelecionada.classList.remove("selecionado");
+
+    //passo 6:
+   informacaoASerMostrada.classList.add("selecionado")}
